437fd67b20d49603a6e47c101584c5e02c81e69a
Uchchash346 <mru416@gmail.com>
2022-07-12 07:16:39 +0600

    Setup screens and functionality

---
+++ components/HomeHeader.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+
+
+const HomeHeader = () => {
+  return (
+    <View>
+      <Text>HomeHeader</Text>
+    </View>
+  )
+}
+
+export default HomeHeader
---
+++ components/index.js
@@ -0,0 +1,7 @@
+import HomeHeader from './HomeHeader';
+import NFTCard from './NFTCard';
+import FocusedStatusBar from './FocusedStatusBar';
+import { CircleButton, RectButton } from './Button';
+export {
+    HomeHeader, NFTCard, FocusedStatusBar, CircleButton, RectButton
+}
