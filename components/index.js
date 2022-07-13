287e5bbf4bca13ff90e05ac067ab9565b1df2152
Uchchash346 <mru416@gmail.com>
2022-07-14 02:07:44 +0600

    Setup some pages

--- components/index.js
+++ components/index.js
@@ -2,6 +2,8 @@ import HomeHeader from './HomeHeader';
 import NFTCard from './NFTCard';
 import FocusedStatusBar from './FocusedStatusBar';
 import { CircleButton, RectButton } from './Button';
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
 export {
-    HomeHeader, NFTCard, FocusedStatusBar, CircleButton, RectButton
+    HomeHeader, NFTCard, FocusedStatusBar, CircleButton, RectButton, DetailsDesc, DetailsBid
 }
--- screens/Details.js
+++ screens/Details.js
@@ -1,7 +1,9 @@
+import { View, Text, SafeAreaView, Image, StatusBar, FlatList } from "react-native";
+import { COLORS, SIZES, assets, SHADOWS, FONTS } from "../constants";
+import { CircleButton, RectButton, SubInfo, DetailsDesc, DetailsBid, FocusedStatusBar } from "../components";
 
-import { Text } from 'react-native'
-
-const Details = () => {
+const Details = ({ route, navigation }) => {
+  console.log(route);
   return (
     <Text>Details</Text>
   )
